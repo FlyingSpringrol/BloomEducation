57c7b2ff0f65b7a1c4f0afeed508bce0410eb6e2
Brian Aronowitz <brian@learnwithjuni.com>
2021-06-16 12:03:33 -0700

    yay data entry

---
+++ Explorer-books-web/cambrian/data.js
@@ -0,0 +1,436 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-awl8kcolor4k",
+      "name": "awl8kcolor4k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hallucengia",
+      "name": "Hallucengia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.24460392887221616,
+          "pitch": 0.04698351482478458,
+          "title": "Hallucigenia",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.202271680289348,
+          "pitch": 0.026715820437679838,
+          "title": "Sponge1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-trilobites4k",
+      "name": "trilobites4k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.2751037210461078,
+          "pitch": 0.21870085810748918,
+          "title": "Trilobites",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.366844911894436,
+          "pitch": 0.09836097495346152,
+          "title": "Sponge2",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-dinomischus4kcolor",
+      "name": "dinomischus4kColor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.3618228143186233,
+          "pitch": -0.19546064977209454,
+          "title": "Dinosmichus",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.884068557269238,
+          "pitch": 0.35120830545651316,
+          "title": "BlackGroundThing",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-marella4k-color",
+      "name": "marella4k-color",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Marella",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8958452364058385,
+          "pitch": 0.02626789038152033,
+          "title": "Anenome thing",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-actaeus4k-color",
+      "name": "actaeus4k-color",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.29870719861259687,
+          "pitch": -0.07648711604971403,
+          "title": "Sponge3<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0377969968187157,
+          "pitch": -0.13110691845386135,
+          "title": "Actaeus",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-sanctacaris4k-color",
+      "name": "sanctacaris4k-color",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.05767105583242227,
+          "pitch": -0.35565595649660864,
+          "title": "Sancatacaris",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-anomalocaris4k-color",
+      "name": "anomalocaris4k-color",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.019242632607859633,
+          "pitch": -0.4219945951873214,
+          "title": "Anomala",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7088749965715806,
+          "pitch": -0.20975050474933,
+          "title": "Nectocaris",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.609138592049927,
+          "pitch": -0.029548009357561966,
+          "title": "Sponge3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-opabinia4k-color",
+      "name": "opabinia4k-color",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5163521733471743,
+          "pitch": -0.09455428720919201,
+          "title": "Opabinia",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7350759191631013,
+          "pitch": 0.22178218498737046,
+          "title": "Ottoia",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-pikaia4kcolor",
+      "name": "pikaia4kColor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.6479476798629342,
+          "pitch": -0.04344873656259196,
+          "title": "Pikaia<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.460576199983045,
+          "pitch": -0.24887974902873822,
+          "title": "Green thing",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-theend4kcolor",
+      "name": "theEnd4kcolor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.2042825435928357,
+          "pitch": 0.03891837649149288,
+          "title": "Authors Note",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ Explorer-books-web/mesozoic-seas/index.js
@@ -0,0 +1,302 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+(function() {
+  var Marzipano = window.Marzipano;
+  var bowser = window.bowser;
+  var data = window.APP_DATA;
+  var viewIdx = 0;
+  var sceneStories = [];
+
+  // Grab elements from DOM.
+  var panoElement = document.querySelector('#pano');
+  const turnOnActiveStory = (viewIdx) =>{
+    turnOffActiveModal();
+    sceneStories[viewIdx].classList.remove('hidden');
+    console.log(sceneStories[viewIdx]);
+  }
+  const turnOffActiveModal = () =>{
+    document.body.querySelectorAll('.info-hotspot-modal').forEach(element => {
+      (element.classList.add('hidden'))
+    });
+  }
+  const generateHomeButton = () =>{
+    var home = document.createElement('a');
+    home.href = "../index.html"
+    home.classList.add('exit-button');
+    document.body.appendChild(home);
+  }
+  generateHomeButton();
+
+  const generateNavbar = () =>{
+    var bottomBar = document.createElement('div');
+    bottomBar.classList.add('bottom-bar');
+    var bottomBarLayout = document.createElement('div');
+    bottomBarLayout.classList.add('bottom-bar-layout');
+  
+    var left = document.createElement('div');
+    left.classList.add('page-button');
+
+    var center= document.createElement('div');
+    center.classList.add('page-button');
+
+    var right = document.createElement('div');
+    right.classList.add('page-button');
+    left.addEventListener('click', ()=>{
+      viewIdx+=1;
+      switchScene(scenes[viewIdx]);
+    })
+    right.addEventListener('click', ()=>{
+      viewIdx-=1;
+      switchScene(scenes[viewIdx]);
+    })
+    center.addEventListener('click', ()=>{
+      turnOnActiveStory(viewIdx);
+    })
+    bottomBarLayout.appendChild(right);
+    bottomBarLayout.appendChild(center);
+
+    bottomBarLayout.appendChild(left);
+    bottomBar.appendChild(bottomBarLayout);
+    document.body.appendChild(bottomBar);  
+  }
+  generateNavbar();
+  // Detect desktop or mobile mode.
+  if (window.matchMedia) {
+    var setMode = function() {
+      if (mql.matches) {
+        document.body.classList.remove('desktop');
+        document.body.classList.add('mobile');
+      } else {
+        document.body.classList.remove('mobile');
+        document.body.classList.add('desktop');
+      }
+    };
+    var mql = matchMedia("(max-width: 500px), (max-height: 500px)");
+    setMode();
+    mql.addListener(setMode);
+  } else {
+    document.body.classList.add('desktop');
+  }
+
+  // Detect whether we are on a touch device.
+  document.body.classList.add('no-touch');
+  window.addEventListener('touchstart', function() {
+    document.body.classList.remove('no-touch');
+    document.body.classList.add('touch');
+  });
+
+  // Use tooltip fallback mode on IE < 11.
+  if (bowser.msie && parseFloat(bowser.version) < 11) {
+    document.body.classList.add('tooltip-fallback');
+  }
+
+  // Viewer options.
+  var viewerOpts = {
+    controls: {
+      mouseViewMode: data.settings.mouseViewMode,
+    }
+  };
+
+  // Initialize viewer.
+  var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
+
+  // Create scenes.
+  var scenes = data.scenes.map(function(data) {
+    var urlPrefix = "tiles";
+    var source = Marzipano.ImageUrlSource.fromString(
+      urlPrefix + "/" + data.id + "/{z}/{f}/{y}/{x}.jpg",
+      { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" });
+    var geometry = new Marzipano.CubeGeometry(data.levels);
+
+    var limiter = Marzipano.RectilinearView.limit.traditional(data.faceSize, 100*Math.PI/180, 120*Math.PI/180);
+    var view = new Marzipano.RectilinearView(data.initialViewParameters, limiter);
+
+    var scene = viewer.createScene({
+      source: source,
+      geometry: geometry,
+      view: view,
+      pinFirstLevel: true
+    });
+
+    // Create info hotspots.
+    data.infoHotspots.forEach(function(hotspot) {
+      var element = createInfoHotspotElement(hotspot);
+      scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
+    });
+
+    sceneStories.push(createStoryModal(data.storyInfo));
+
+    return {
+      data: data,
+      scene: scene,
+      view: view
+    };
+  });
+
+  function switchScene(scene) {
+    turnOffActiveModal();
+    scene.view.setParameters(scene.data.initialViewParameters);
+    scene.scene.switchTo();
+  }
+
+
+  function createStoryModal(storyInfo){
+    var modalContent = document.createElement('div');
+    modalContent.classList.add('info-hotspot-modal-content');
+
+    const title = document.createElement('h1');
+    title.innerHTML = storyInfo.title;
+
+    var modal = document.createElement('div');
+    modal.classList.add('info-hotspot-modal');
+    modal.classList.add('hidden');
+
+    document.body.appendChild(modal);
+    var closeWrapper = document.createElement('div');
+    closeWrapper.classList.add('close-button-wrapper');
+    closeWrapper.innerHTML = 'x';
+    modalContent.appendChild(closeWrapper);
+    modalContent.appendChild(title);
+
+    for (var i = 0; i < storyInfo.paragraphs.length; i++){
+      var content = storyInfo.paragraphs[i];
+      var par = document.createElement('p');
+      par.innerHTML = content;
+      modalContent.appendChild(par);
+    }
+    modal.appendChild(modalContent);
+
+    var logo = document.createElement('img');
+    logo.src = '../img/logo.png';
+    logo.classList.add('logo');
+    modalContent.appendChild(logo);
+
+    closeWrapper.addEventListener('click', ()=>{
+      modal.classList.add('hidden');
+    })
+
+    return modal;
+
+  }
+
+  function createInfoHotspotElement(hotspot) {
+
+    // Create wrapper element to hold icon and tooltip.
+    var hotspotWrapper = document.createElement('div');
+    hotspotWrapper.classList.add('hotspot');
+    hotspotWrapper.classList.add('info-hotspot');
+    // Create image element.
+    var iconWrapper = document.createElement('div');
+    iconWrapper.classList.add('info-hotspot-icon-wrapper');
+    var icon = document.createElement('img');
+    icon.src = 'img/magnifiyingGlass.png';
+    icon.classList.add('info-hotspot-icon');
+    iconWrapper.appendChild(icon);
+
+    hotspotWrapper.appendChild(iconWrapper);
+
+
+
+    //Do the modal stuff
+    var modalContent = document.createElement('div');
+    modalContent.classList.add('info-hotspot-modal-content');
+
+
+
+    var closeWrapper = document.createElement('div');
+    closeWrapper.classList.add('close-button-wrapper');
+    closeWrapper.innerHTML = 'x';
+
+    // title, body, image
+    var modalTitle = document.createElement('h1');
+    modalTitle.innerHTML = hotspot.title;
+
+    var modalSize = document.createElement('p');
+    modalSize.innerHTML = hotspot.size;
+
+    var modalBody = document.createElement('div');
+
+    if (hotspot.paragraphs){
+      for (var i = 0; i < hotspot.paragraphs.length; i++){
+        var par = document.createElement('p');
+        par.innerHTML = hotspot.paragraphs[i];
+        modalBody.appendChild(par);
+      }
+    }
+
+    var image = document.createElement('img');
+    image.classList.add('modal-image');
+    image.src = hotspot.image;
+
+    // Create a modal for the hotspot content to appear on mobile mode.
+    var modal = document.createElement('div');
+    modal.classList.add('info-hotspot-modal');
+    modal.classList.add('hidden');
+    document.body.appendChild(modal);
+    modalContent.appendChild(closeWrapper);
+    modalContent.appendChild(modalTitle);
+    modalContent.appendChild(image);
+    modalContent.appendChild(modalSize);
+    modalContent.appendChild(modalBody);
+
+    if (hotspot.fossil){
+      var citationWrapper = document.createElement('div');
+      citationWrapper.classList.add('citation-wrapper')
+      var fossil = document.createElement('img');
+      fossil.classList.add('citation-image');
+      fossil.src = hotspot.fossil.url;
+      var citation = document.createElement('p');
+      citation.innerHTML = hotspot.fossil.citation;
+      var link = document.createElement('a');
+      link.innerHTML= hotspot.fossil.citationUrl;
+      link.href = hotspot.fossil.citationUrl;
+      citationWrapper.appendChild(fossil);
+      citationWrapper.appendChild(citation);
+      citationWrapper.appendChild(link);
+      modalContent.appendChild(citationWrapper);
+    }
+
+
+    modal.appendChild(modalContent);
+    hotspotWrapper.addEventListener('click', ()=>{
+      turnOffActiveModal();
+      modal.classList.remove('hidden');
+    })
+    closeWrapper.addEventListener('click', ()=>{
+      modal.classList.add('hidden');
+    })
+    stopTouchAndScrollEventPropagation(hotspotWrapper);
+    return hotspotWrapper;
+  }
+
+
+  // Prevent touch and scroll events from reaching the parent element.
+  function stopTouchAndScrollEventPropagation(element, eventList) {
+    var eventList = [ 'touchstart', 'touchmove', 'touchend', 'touchcancel',
+                      'wheel', 'mousewheel' ];
+    for (var i = 0; i < eventList.length; i++) {
+      element.addEventListener(eventList[i], function(event) {
+        event.stopPropagation();
+      });
+    }
+  }
+
+  // Display the initial scene.
+  switchScene(scenes[viewIdx]);
+
+})();
